43eaf0ae70893cc17245591dee4657b0eef674f5
Ajay Jakate <ajay.jakate@gmail.com>
2021-10-23 16:33:42 -0500

    basic token and refresh

--- src/clj/basha/auth.clj
+++ src/clj/basha/auth.clj
@@ -4,6 +4,7 @@
    [next.jdbc :as jdbc]
    [basha.db.core :as db]
    [buddy.sign.jwt :as jwt]
+   [java-time :as t]
    [basha.config :refer [env]]))
 
 ;; (defonce token-secret (select-keys env [:token-secret]))
@@ -19,5 +20,8 @@
   (let [user (db/get-user-for-login {:username login})
         authenticated (hashers/check password (:pass user))]
     (if authenticated
-      {:access-token (jwt/sign {:user login} "token-secret")}
+      {:access-token (jwt/sign {:user login} "token-secret"
+                               {:exp   (t/plus (t/instant) (t/hours 1))})
+       :refresh-token (jwt/sign {:user login} "token-secret"
+                                {:exp   (t/plus (t/instant) (t/days 1))})}
       {})))
