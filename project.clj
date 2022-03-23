349b316af5f28ac4d74dcf200e2bcaaf609a0f18
Ajay Jakate <ajay.jakate@gmail.com>
2022-03-22 23:53:49 -0500

    try sass

--- project.clj
+++ project.clj
@@ -51,31 +51,44 @@
                  [akiroz.re-frame/storage "0.1.4"]]
 
   :min-lein-version "2.0.0"
-  
+
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
   :test-paths ["test/clj"]
   :resource-paths ["resources" "target/cljsbuild"]
   :target-path "target/%s/"
   :main ^:skip-aot basha.core
 
-  :plugins [] 
+  :plugins [[lein-scss "0.3.0"]]
+
   :clean-targets ^{:protect false}
   [:target-path "target/cljsbuild"]
   
+  :hooks [leiningen.scss]
+
+  :scss {:builds
+         {:development {:source-dir "resources/scss"
+                        :dest-dir "resources/public/css"
+                        :executable "sass"
+                        :args ["--style" "expanded"]}
+          :production {:source-dir "resources/scss"
+                       :dest-dir "resources/public/css"
+                       :executable "sass"
+                       :args ["--style" "compressed"]
+                       :jar true}}}
 
   :profiles
   {:uberjar {:omit-source true
-             
+
              :prep-tasks ["compile" ["run" "-m" "shadow.cljs.devtools.cli" "release" "app"]]
              :aot :all
              :uberjar-name "basha.jar"
-             :source-paths ["env/prod/clj"  "env/prod/cljs" ]
+             :source-paths ["env/prod/clj"  "env/prod/cljs"]
              :resource-paths ["env/prod/resources"]}
 
    :dev           [:project/dev :profiles/dev]
    :test          [:project/dev :project/test :profiles/test]
 
-   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn" ]
+   :project/dev  {:jvm-opts ["-Dconf=dev-config.edn"]
                   :dependencies [[binaryage/devtools "1.0.3"]
                                  [cider/piggieback "0.5.2"]
                                  [org.clojure/tools.namespace "1.1.0"]
@@ -86,10 +99,10 @@
                                  [ring/ring-mock "0.4.0"]]
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]
                                  [jonase/eastwood "0.3.5"]
-                                 [cider/cider-nrepl "0.26.0"]] 
-                  
-                  
-                  :source-paths ["env/dev/clj"  "env/dev/cljs" "test/cljs" ]
+                                 [cider/cider-nrepl "0.26.0"]]
+
+
+                  :source-paths ["env/dev/clj"  "env/dev/cljs" "test/cljs"]
                   :resource-paths ["env/dev/resources"]
                   :repl-options {:init-ns user
                                  :timeout 120000}
