a439683c66adc1bf74a801e0f02a41a9786188d6
Ajay Jakate <ajay.jakate@gmail.com>
2022-08-24 22:27:24 -0500

    delte modal

--- src/cljs/basha/modals/delete.cljs
+++ src/cljs/basha/modals/delete.cljs
@@ -4,6 +4,7 @@
    [re-frame.core :as rf]
    [basha.events]))
 
+;; TODOO: add icon
 (defn delete-modal [sub-delete-id clear-delete-id delete-func name-key]
   (let [active @(rf/subscribe [sub-delete-id])
         id (:id active)
@@ -12,15 +13,15 @@
       [:div.modal
        {:class (if active "is-active" nil)}
        [:div.modal-background]
-       [:div.model-content>div.card
-        [:header.card-header>p.card-header-title "Confirm Delete"]
-        [:div.card-content.has-text-centered
+       [:div.model-content>div.card.has-background-white.has-text-centered.p-4
+        [:h2.has-text-red.is-size-3.mb-4 "Confirm Delete"]
+        [:div
          [:span "Are you sure you want to delete "]
          [:span.has-text-weight-bold name]
          [:span "?"]
-         [:p "This action is irreversible..."]
-         [:div.columns.mt-4
-          [:div.column.control>button.button.is-danger
-           {:on-click #(rf/dispatch [delete-func id])} "Delete"]
-          [:div.column.control>button.button
-           {:on-click #(rf/dispatch [clear-delete-id])} "Cancel"]]]]])))
+         [:p "We won't be able to restore it if you change your mind."]
+         [:div.flex.is-justify-content-center.mt-4
+          [:button.button.m-3
+           {:on-click #(rf/dispatch [clear-delete-id])} "Cancel"]
+          [:button.button.m-3.is-danger
+           {:on-click #(rf/dispatch [delete-func id])} "Delete"]]]]])))
