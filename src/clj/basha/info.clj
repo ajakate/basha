f5f0f9ccd15434e5629018d06b17691bfbc84a09
Ajay Jakate <ajay.jakate@gmail.com>
2022-08-31 19:52:05 -0500

    cleanup

--- src/clj/basha/info.clj
+++ src/clj/basha/info.clj
@@ -3,16 +3,6 @@
    [basha.db.core :as db]
    [honey.sql :as sql]))
 
-;; (defn fetch []
-;;   (db/execute-one
-;;    (sql/format
-;;     {:select [[:username :admin]
-;;               [{:select [[[:count :*]]] :from :users} :total_users]]
-;;      :from :users
-;;      :order-by [[:created_at :asc]]
-;;      :limit 1})))
-
-
 (defn fetch []
   (let [response (db/execute-one
                   (sql/format
