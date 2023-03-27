88445aabf42bad805617404d999c1332aa7aa0da
Ajay Jakate <ajay.jakate@gmail.com>
2023-03-26 19:14:23 -0500

    tailwind works in dev mode

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    // in prod look at shadow-cljs output file in dev look at runtime, which will change files that are actually compiled; postcss watch should be a whole lot faster
+    // TODON: fix this
+    // content: process.env.NODE_ENV == 'production' ? ["target/cljsbuild/public/js/app.js"] : ["./resources/public/js/cljs-runtime/*.js"],
+    content: ["target/cljsbuild/public/js/app.js"],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
+}
