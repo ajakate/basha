711115f710d13ab59a0b6b7b27396cdddc366513
Ajay Jakate <ajay.jakate@gmail.com>
2023-03-27 14:13:34 -0500

    semibold doesn't do anything

--- src/cljs/basha/pages/dashboard.cljs
+++ src/cljs/basha/pages/dashboard.cljs
@@ -12,7 +12,7 @@
    [:a.link>div.mt-1 "Get Help →"]])
 
 (defn list-summary-table [lists]
-  [:div.table-container.m-4.my-6>table.table.is-fullwidth
+  [:div.table-container.m-4.mb-6>table.table.is-fullwidth.is-hidden-mobile
    [:thead.has-background-blue
     [:tr.has-text-white
      [:th.has-text-white "Language"]
@@ -48,11 +48,47 @@
                {:on-click #(rf/dispatch [:set-delete-list-id list])}
                "Delete"]])]]]))]])
 
+(defn list-summary-table-mobile [lists]
+  [:div.table-container>table.table.is-fullwidth.is-hidden-tablet
+   [:thead.has-background-blue
+    [:tr.has-text-white
+     [:th.has-text-white "List Info"] 
+     [:th.has-text-white "Progress"]
+     [:th]]]
+   [:tbody
+    (for [list lists]
+      ^{:key (:id list)}
+      (let [done_count (- (:list_count list) (:open_count list))
+            percentage (* 100 (/ done_count (:list_count list)))]
+        [:tr
+         [:td.columns.is-gapless
+          [:div.column.mb-1.bold (:name list)]
+          [:div.column.mb-4.is-italic (:target_language list)]
+          [:div.column
+           [:div
+            [:p.bold "Shared with:"]
+            [:p (:users list)]]]]
+         [:td
+          [:progress.progress.is-green.my-3 {:value percentage :max "100"}]
+          [:p (str done_count "/" (:list_count list) " items translated")]]
+         [:td
+          [:div.columns.is-gapless
+           [:a.link.has-text-orange.px-2.my-4.column
+            {:href (str "/#/lists/edit/" (:id list))}
+            "Edit"]
+           [:a.link.has-text-orange.px-2.my-4.column
+            {:on-click #(rf/dispatch [:set-downloading-deck (:id list) (:name list)])}
+            "Download"]
+           (when (is-admin)
+             [:a.link.has-text-orange.px-2.my-4.column
+              {:on-click #(rf/dispatch [:set-delete-list-id list])}
+              "Delete"])]]]))]])
+
 (defn dashboard-page []
   (let [lists @(rf/subscribe [:list-summary])]
-    [:div>div.basha-panel.mx-auto.background-semi-faded.p-5
+    [:div.basha-panel.mx-auto.background-semi-faded
      [loading-screen :loading-list-summary]
-     [:div.columns
+     [:div.columns.p-4
       [:div.is-size-3.bold.mb-3.column "My Dashboard"]
       (when (is-admin)
         [:div
@@ -64,7 +100,9 @@
           [:a.button.is-orange.bold
            {:on-click #(rf/dispatch [:create-deck-modal-visible true])}
            "Add New Deck"]]])]
-     [:p "Use this hub to upload your files, manage translations, and export decks."]
+     [:div.columns.p-4>p.column.py-0 "Use this hub to upload your files, manage translations, and export decks."]
      (if (seq lists)
-       [list-summary-table lists]
+       [:div
+        [list-summary-table lists]
+        [list-summary-table-mobile lists]]
        [no-lists])]))
